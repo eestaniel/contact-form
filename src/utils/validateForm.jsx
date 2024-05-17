0cbbdd29bd51148402437968e7d36bc3e49f0727
EEstaniel <edwineestaniel93@gmail.com>
2024-05-17 05:06:03 -0700

    fix spacing

--- src/utils/validateForm.jsx
+++ src/utils/validateForm.jsx
@@ -3,13 +3,13 @@ export const validateForm = (formData) => {
   const errors = {};
   if (!formData.firstName) errors.firstName = "This field is required";
   if (!formData.lastName) errors.lastName = "This field is required";
-    if (!formData.email) {
-      errors.email = "This field is required";
-    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      errors.email = "Please enter a valid email";
-    }
-    if (!formData.queryType) errors.queryType = "Please select a query type";
-    if (!formData.message) errors.message = "This field is required";
-    if (!formData.consent) errors.consent = "To submit this form, please consent to being contacted";
+  if (!formData.email) {
+    errors.email = "This field is required";
+  } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    errors.email = "Please enter a valid email";
+  }
+  if (!formData.queryType) errors.queryType = "Please select a query type";
+  if (!formData.message) errors.message = "This field is required";
+  if (!formData.consent) errors.consent = "To submit this form, please consent to being contacted";
   return errors;
 };
